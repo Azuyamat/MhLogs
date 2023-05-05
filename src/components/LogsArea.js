5c3c188f375facd6bf326e578cccae2a61139c23
Azuyamat <discordazu@gmail.com>
2023-05-05 00:28:34 -0400

    QA commit

--- src/components/LogsArea.js
+++ src/components/LogsArea.js
@@ -1,6 +1,8 @@
 import styles from "@/styles/LogsArea.module.css"
 import React, {useEffect, useState} from 'react';
 import {randomInt} from "next/dist/shared/lib/bloom-filter/utils";
+import {AiFillWarning, AiFillInfoCircle} from "react-icons/ai";
+import {FaServer} from "react-icons/fa";
 
 let construction = [];
 let serverVersion = 0;
@@ -11,6 +13,7 @@ function LogsArea(props){
     const notDefined = "Not defined"
     const [logs, setLogs] = useState(<div>text</div>)
     const [version, setVersion] = useState(0)
+    const [errors, setErrors] = useState(0)
 
     function analyze(text){
         construction = [];
@@ -27,15 +30,22 @@ function LogsArea(props){
                         if (e.key === 'Enter'){
                             e.preventDefault()
                             analyze(e.target.value)
-                            console.log("Enter")
                         }
+                    }} onPaste={(e) => {
+                        setTimeout(function(){
+                            analyze(e.target.value)
+                        }, 100)
                     }} spellCheck={false}/>
             </div>
             <div className={styles.ctn}>
                 <div className={styles.serverInfo}>
                     <ul>
-                        <li>Server Version: <span className={styles.highlight} id={"version"}>{version}</span></li>
-                        <li>Server Type: <span className={styles.highlight} id={"longVersion"}>{version}</span></li>
+                        <li><span className={styles.icon} style={{color:"#4bff89"}}><FaServer/></span> Server Version: <span className={styles.highlight} id={"version"}>{version}</span></li>
+                        <li><span className={styles.icon} style={{color:"#4bff89"}}><FaServer/></span> Server Type: <span className={styles.highlight} id={"longVersion"}>{version}</span></li>
+                        <li><span className={styles.icon} style={{color:"red"}}><AiFillWarning/></span> Errors: <span className={styles.highlight} id={"errors"} style={{color:"red"}}> {errors}</span></li>
+                        <li><span className={styles.icon} style={{color:"#4bff89"}}><AiFillInfoCircle/></span> Website it still in <span className={styles.highlight} id={"errors"}>beta</span></li>
+                        <li><span className={styles.icon} style={{color:"#4bff89"}}><AiFillInfoCircle/></span> Made by <span className={styles.highlight} style={{textDecoration:'underline'}}><a
+                            href="https://azuyamat.com">Azuyamat</a></span></li>
                     </ul>
                 </div>
                 <Results logs={logs} id={randomInt(0, 999999)}/>
@@ -47,6 +57,7 @@ function LogsArea(props){
     function Results(props){
         const text = props.logs.props.children
         const split = text.split("\n");
+        let er = 0;
         for (let s in split){
             const t = split[s]
             const array = t.match(new RegExp("(?<time>^[[]\\d\\d:\\d\\d:\\d\\d])(?<text>.+)", "gm"))
@@ -72,6 +83,7 @@ function LogsArea(props){
                 let regex = /(\w+|\d+|\D)([[]\/\d{0,3}.\d{0,3}.\d{0,3}.\d{0,3}:\d{0,7}])/gm
                 content = content.replace(regex, "REDACTED IP")
                 if (type === "ERROR"){
+                    er++
                     if (content.includes("Could not load plugin")){
                         reason = "An error is occurring with the plugin mentioned above. Either try to" +
                             " fix it or switch to another plugin"
@@ -88,6 +100,7 @@ function LogsArea(props){
                 construction.push(<Result key={randomInt(0, 999999999999)} line={s} type={type} time={time} reason={reason}>{content}</Result>)
             }
         }
+        setErrors(er)
         return(
             <div id={"construction"}>{construction}</div>
         )
@@ -96,6 +109,7 @@ function LogsArea(props){
     function Result(props){
         return (
             <div className={styles.line} data-type={props.type}>
+                {props.type === "ERROR" ? <span className={styles.icon} style={{color:"red"}}><AiFillWarning/></span> : ""}
                 <p>{props.line}</p>
                 <span>{props.children}</span>
                 {props.type === "ERROR" ? <div className={styles.moreInfo}>{props.reason}</div> : ""}
